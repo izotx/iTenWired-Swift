c3cf0c876ebafeb2a71b8403ecf84ba2e1838dcc
Janusz Chudzynski <janusz@izotx.com>
2016-07-20 13:10:54 -0500

    Refactoring

--- Conference App/NearMeCollectionViewCell.swift
+++ Conference App/NearMeCollectionViewCell.swift
@@ -1,4 +1,4 @@
-//    Copyright (c) 2016, UWF
+//    Copyright (c) 2016, Izotx
 //    All rights reserved.
 //
 //    Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
 //    * Redistributions in binary form must reproduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
-//    * Neither the name of UWF nor the names of its contributors may be used to
+//    * Neither the name of Izotx nor the names of its contributors may be used to
 //    endorse or promote products derived from this software without specific
 //    prior written permission.
 //
